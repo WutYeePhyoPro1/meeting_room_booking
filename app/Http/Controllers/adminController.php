821e293d77735b185e3b3729a3a402a3032f5e1e
Kyaw Zaww <kzaww4422@gmail.com>
2023-11-09 13:52:29 +0630

    0911finalupdate

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Branch;
+use App\Models\Reason;
 use App\Models\Booking;
 use App\Rules\ColorRule;
 use App\Models\RoomImage;
 use App\Models\Department;
 use App\Models\MeetingRoom;
 use Illuminate\Http\Request;
+use App\Models\BookingRequest;
 use Illuminate\Validation\Rule;
 use App\Rules\RoomNameDublicate;
 use App\Customize\Commonfunction;
-use App\Models\BookingRequest;
-use App\Models\Reason;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\CssSelector\Node\FunctionNode;
@@ -24,7 +25,78 @@ class adminController extends Controller
 {
     public function dashboard(){
         if(getAuth()->employee_id == '000-000000'){
-            return view('admin.home');
+            $this_year = Carbon::now()->format('Y');
+            $last_year = Carbon::now()->subYear(1)->format('Y');
+            $data = Booking::selectRaw('TO_CHAR(date,\'Month\')  as month, count(date) as count')
+                            ->whereYear('date',$this_year)
+                            ->groupBy(DB::raw('TO_CHAR(date,\'Month\') '))
+                            ->orderBy('month')
+                            ->get();
+
+            $data1 = Booking::selectRaw('TO_CHAR(date,\'Month\')  as month, count(date) as count')
+                            ->whereYear('date',$last_year)
+                            ->groupBy(DB::raw('TO_CHAR(date,\'Month\') '))
+                            ->orderBy('month')
+                            ->get();
+
+            if(request('month')){
+                $year = date('Y',strtotime(request('month')));
+                $month = date('m',strtotime(request('month')));
+
+            }
+            $user_data = Booking::with('user')->select(DB::raw('count(user_id) as count'),'user_id')
+                                    ->when(request('month'),function($q) use($year,$month){
+                                        $q->whereMonth('date',$month)
+                                        ->whereYear('date',$year);
+                                    })
+                                    ->when(request('from_date') && !request('month'),function($q){
+                                        $q->where('date','>=',request('from_date'));
+                                    })
+                                    ->when(request('to_date') && !request('month'),function($q){
+                                        $q->where('date','၊=',request('to_date'));
+                                    })
+                                    ->groupBy('user_id')
+                                    ->orderBy('user_id')
+                                    ->withTrashed()
+                                    ->get();
+            $all_data = Booking::when(request('month'),function($q) use($year,$month){
+                                    $q->whereMonth('date',$month)
+                                    ->whereYear('date',$year);
+                                })
+                                ->when(request('from_date') && !request('month'),function($q){
+                                    $q->where('date','>=',request('from_date'));
+                                })
+                                ->when(request('to_date') && !request('month'),function($q){
+                                    $q->where('date','၊=',request('to_date'));
+                                })
+                                ->withTrashed()
+                                ->get();
+            $all_user = User::whereNot('employee_id','000-000000')->orderBy('id')->get();
+            $user = $all_user->pluck('name')->all();
+            $color = $all_user->pluck('bg_color')->all();
+            $data_user = [];
+            $final_data= [];
+            foreach($all_user as $item){
+                $data_user[$item->name] = null;
+            }
+            foreach($user_data as $item)
+            {
+                    $data_user[$item->user->name] = $item->count;
+            }
+            foreach($data_user as $index=>$item){
+                $final_data[]=$item;
+            }
+
+            $this_year_data = ['January' => null, 'February' => null, 'March' => null, 'April' => null, 'May' => null, 'June' => null, 'July' => null,'August' => null,'September' => null,'October' => null,'November' => null,'December' => null];
+            $last_year_data = $this_year_data;
+            foreach($data as $item){
+                $this_year_data[trim($item->month,' ')] = $item->count;
+            }
+            foreach($data1 as $item){
+                $last_year_data[trim($item->month,' ')] = $item->count;
+            }
+
+            return view('admin.home',compact('this_year_data','last_year_data','final_data','user_data','user','color','all_data'));
         }else{
             $room = MeetingRoom::orderBy('created_at','asc')->get();
             $current_month = Carbon::now()->format('m');
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,69 +1,132 @@
 @extends('new_layouts.admin.layout')
 @section('content')
-    <div class="p-4">
-        Dashboard
+    <div class="ps-5">
+        <div class="p-4">
+            <span class="text-2xl font-serif">Dashboard</span>
+        </div>
+        <div class="mx-auto shadow-lg px-5 pb-2 bg-slate-100 shadow-slate-400" style="position: relative; height:45vh; width:95%">
+            <canvas id="myChart" style="width: 100%"></canvas>
+        </div>
+        <div id="this_year" data-item="{{ json_encode($this_year_data) }}"></div>
+        <div id="last_year" data-item="{{ json_encode($last_year_data) }}"></div>
     </div>
-    <div class="" style="width: 500px">
-        <canvas id="myChart"></canvas>
+    <div class="ps-5 my-10 grid grid-cols-3 gap-5">
+        <div class=" ms-10 shadow-lg p-5 bg-slate-100 shadow-slate-400" style="position: relative; height:45vh; width:100%">
+            <canvas id="myChart1" style="width: 100%"></canvas>
+            <input type="hidden" id="user" data-item="{{ json_encode($user) }}">
+            <input type="hidden"  id="data_user" data-item="{{ json_encode($final_data) }}">
+            <input type="hidden"  id="color" data-item="{{ json_encode($color) }}">
+        </div>
+        <div class="col-span-2 ms-10">
+            <div class="">
+                <form action="{{ route('home') }}" method="GET">
+                    <div class="p-5 grid grid-cols-4 gap-8">
+                        <div class="flex flex-col">
+                            <label for="from_date">From Date :</label>
+                            <input type="date" id="from_date" value="{{ request('from_date') }}" name="from_date" class="mt-2 border-1 text-slate-700 border-slate-400 rounded-lg focus:ring-0 focus:border-b-4  focus:border-slate-400 placeholder-slate-300">
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="to_date">To Date :</label>
+                            <input type="date" id="to_date" value="{{ request('to_date') }}" name="to_date" class="mt-2 border-1 text-slate-700 border-slate-400 rounded-lg focus:ring-0 focus:border-b-4  focus:border-slate-400 placeholder-slate-300">
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="status">Month :</label>
+                            <input type="month" id="month" value="{{ request('month') }}" name="month" class="mt-2 border-1 text-slate-700 border-slate-400 rounded-lg focus:ring-0 focus:border-b-4  focus:border-slate-400 placeholder-slate-300">
+                        </div>
+                        <div class="">
+                            <x-button class="bg-emerald-600 text-white w-1/2 mt-8 h-10 ms-5 ps-8 hover:bg-emerald-800">Search</x-button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="grid grid-cols-4 gap-4 px-10">
+                <div class="w-full h-24 bg-slate-200 shadow-lg text-center pt-1 text-xl font-serif flex flex-col">
+                    <span class="underline">All</span>
+                    <span class="mt-3">{{ count($all_data) }}</span>
+                </div>
+                @foreach ($user_data as $item)
+                    <div class="w-full h-24 bg-slate-200 shadow-lg text-center pt-1 text-xl font-serif flex flex-col">
+                        <span class="underline">{{ $item->user->name }}</span>
+                        <span class="mt-3">{{ $item->count }}</span>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
     </div>
     <script>
-  var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August'],
-        datasets: [{
-            label: 'Last Month',
-            data: [15,25, 15, 7, 3, 2, 3, 9],
-            backgroundColor: [
+        const this_year = JSON.parse(document.getElementById('this_year').dataset.item);
+        const last_year = JSON.parse(document.getElementById('last_year').dataset.item);
 
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-        },{
-            label: 'This Month',
-            data: [15,5, 15, 23, 27, 28, 27, 9],
-            backgroundColor: [
+        const user = JSON.parse(document.getElementById('user').dataset.item);
+        const data_user = JSON.parse(document.getElementById('data_user').dataset.item);
+        const color = JSON.parse(document.getElementById('color').dataset.item);
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+                    datasets: [{
+                        fill: {
+                        target: 'origin',
+                        below:  'rgba(255, 159, 64, 0.2)'
+                    },
+                        label: 'This Year',
+                        data: this_year,
+                        backgroundColor: [
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(153, 102, 255, 1)'
+                        ],
+                        borderWidth: 1
+                    },{
+                        fill: {
+                        target: 'origin',
+                        below:  'rgba(255, 99, 132, 0.2)'
+                    },
+                        label: 'Last Year',
+                        data:last_year,
+                        backgroundColor: [
 
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-        }
-        ,{
-            label: 'now',
-            data: [null, null, null, null, null, null ,20, null],
-            backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            options: {
+                maintainAspectRatio : false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Comparism Between This Year And Last Year'
+                    }
+                },
+            }
+        });
 
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+        var ctx1 = document.getElementById('myChart1').getContext('2d');
+        var myChart = new Chart(ctx1, {
+            type: 'doughnut',
+                data: {
+                    labels: user,
+                    datasets: [{
+                        data:data_user,
+                        backgroundColor: color,
+                        borderColor: [
+                            '#ffff'
+                        ],
+                        borderWidth: 3,
+                        hoverOffset: 10,
+                    }]
+                },
+            options: {
+                maintainAspectRatio : false,
+
+            }
+        });
       </script>
 @endsection
